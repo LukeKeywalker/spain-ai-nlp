1f43d69bc0e62dd72deb11a5a43f51013ad216f5
lmichniewicz <lukasz.michniewicz@gmail.com>
2021-02-02 05:36:47 +0100

    resolves #2

--- generate.py
+++ generate.py
@@ -1,14 +1,12 @@
-import os
 import gpt_2_simple as gpt2
-
-model_name = "124M"
-if not os.path.isdir(os.path.join("models", model_name)):
-    print(f"Downloading {model_name} model...")
-    gpt2.download_gpt2(model_name=model_name)
 
 
 sess = gpt2.start_tf_sess()
-gpt2.load_gpt2(sess, model_name=model_name, checkpoint_dir='checkpoint')
+gpt2.load_gpt2(
+    sess,
+    checkpoint_dir='models',
+    run_name='spain-ai-nlp'
+)
 
 
 prompt = "<|startoftext|><|startofdesc|>{}<|endofdesc|><|startofname|>"
