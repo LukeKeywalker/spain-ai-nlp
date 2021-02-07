d003b2d78620b26d252c4f8f90775ad3bf11355a
Cyberosa <arosacastillo@protonmail.com>
2021-02-07 20:26:11 +0100

    Resolve #34 (#35)

--- sorting_utils.py
+++ sorting_utils.py
@@ -8,12 +8,12 @@ def get_nr_words(text):
     return len(tokens)
 
 
-def length_wc_scoring(answer):
+def length_wc_scoring(answer,l_max, w_max):
     """
     Gives a score (range [0,1]) to the answer
     considering two metrics:
-    1. length of the answer: max score 0.5
-    2. number of words: max score 0.5
+    1. length of the answer: max score l_max
+    2. number of words: max score w_max
     Taking the references observed in the train data
     :param answer: the answer to score
     :return: the score between 0 and 1
@@ -23,16 +23,16 @@ def length_wc_scoring(answer):
     l = len(answer)
     # maximum score for length between 19 and 30 chars
     if l in range(19,31):
-        score += 0.5
+        score += l_max
     else: # inverse proportional to how far you are from the avg
         avg = 24
-        score += 0.5/abs(avg - l)
+        score += l_max/abs(avg - l)
     # word count
     wc = get_nr_words(answer)
     # maximum score for 3, 4 or 5 words
     if wc in [3,4,5]:
-        score += 0.5
+        score += w_max
     else: # inverse proportional to how far you are from the avg
         avg = 4
-        score += 0.5/abs(avg - wc)
+        score += w_max/abs(avg - wc)
     return score
